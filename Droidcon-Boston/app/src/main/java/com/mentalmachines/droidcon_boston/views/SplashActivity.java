9c57d5206ca711679f4cf8f20cf97e15372981e6
Nishant Srivastava <nisrulz@gmail.com>
2018-02-11 12:47:10 +0530

    updated: codebase - added MyApplication class and init fabric inside it - added AppConstants - added Splash Screen Activity - updated colors.xml with correct theme colors - updated styles.xml - updated strings.xml

---
+++ Droidcon-Boston/app/src/main/java/com/mentalmachines/droidcon_boston/views/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.mentalmachines.droidcon_boston.views;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+import com.mentalmachines.droidcon_boston.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Make Fullscreen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            finish();
+        }, 1200);
+    }
+}
